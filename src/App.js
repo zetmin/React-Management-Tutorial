ada0c7878dc2836f1a62cbcc2ce0e2fb32fd73a1
zetmin <zzagn@netsgo.com>
2020-05-21 20:49:00 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" / >
+      <h2>Let`s Develop Management System!</h2>
+      
+    </div>
+  );
+}
+
+export default App;
