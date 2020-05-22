69b6aac81b9771ac7ca523d6708ebd24fc621842
zetmin <zzagn@netsgo.com>
2020-05-22 18:10:46 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
+const customers = [
+  {
+  'id': 1 ,
+  'image':'http://placeimg.com/64/64/1',
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id': 2 ,
+  'image':'http://placeimg.com/64/64/2',
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id': 3 ,
+  'image':'http://placeimg.com/64/64/3',
+  'name':'홍길동',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+}
+]
+
 function App() {
+  
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" / >
-      <h2>Let`s Develop Management System!</h2>
-      
+    <div>
+      {
+        customers.map(c=>{
+          return(
+            <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          );
+        })
+      }
     </div>
   );
 }
