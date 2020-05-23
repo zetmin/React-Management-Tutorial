4a11d0358e17d22a174eb6daf7ee46b34cfed0b1
zetmin <zzagn@netsgo.com>
2020-05-23 10:43:38 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 const customers = [
   {
